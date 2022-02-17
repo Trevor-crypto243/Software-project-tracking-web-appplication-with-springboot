79dca47dee9b0f253ccbf2cdfebdaeaadcb5c9c1
Trevor-crypto243 <82540204+Trevor-crypto243@users.noreply.github.com>
2022-02-17 23:47:39 +0300

    Add files via upload

---
+++ Devtrac1Application.java
@@ -0,0 +1,13 @@
+package com.houser.devtrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devtrac1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devtrac1Application.class, args);
+	}
+
+}
---
+++ ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.houser.devtrac;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Devtrac1Application.class);
+	}
+
+}
